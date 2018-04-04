216dbb8a3a09f9b8635c23dc01c3064ea07634d3
nimrodPeru <nimrod2410@gmail.com>
2018-04-04 18:21:31 -0500

    Sprint2

--- script.js
+++ script.js
@@ -70,6 +70,33 @@ function estadistica(){
 }
 
 
+
+function pintar(e){
+    
+  // e.style.cssText = "background-color:pink;color:blue; border:border ";
+    e.style.cssText="border: 2px solid #54bac2!important;color:  #54bac2!important";
+   // e.style.cssText="border: 2px solid #DDDDDD!important";
+
+}
+
+function despintar(e){
+    e.style.cssText="border: 2px solid #DDDDDD!important";
+}
+
+function pintarCurso(e){
+    
+  // e.style.cssText = "background-color:pink;color:blue; border:border ";
+ // e.style.cssText="color:  #54bac2!important";
+
+}
+
+function curso1(){
+    //e.style.cssText="color:  #DDDDDD";
+    //window.location.assign("https://www.w3schools.com")
+    location.href="curso1.html"
+    
+}
+
 $(document).ready(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
