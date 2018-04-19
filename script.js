0089b4128c4f993adcba9e690a2fb8b32bd637b5
nimrodPeru <nimrod2410@gmail.com>
2018-04-19 11:30:20 -0500

    commit 20180419

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ function curso1(){
     
 }
 
+
 $(document).ready(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
